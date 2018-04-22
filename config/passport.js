c3915ea2a6448900b3c46cda66e483d079ab8c39
Kumar Rohit Malhotra <krohitm@gmail.com>
2018-04-22 15:39:21 -0400

    set access to config vars

--- config/passport.js
+++ config/passport.js
@@ -3,12 +3,22 @@ const mongoose = require('mongoose');
 //const keys = require('./keys');
 
 
+//setting access to config vars
+if (process.env.NODE_ENV === 'production') {
+  clientID = process.env.googleClientID;
+  clientSecret = process.env.googleClientSecret
+}
+else {
+  const keys = require('./keys');
+  clientID = keys.googleClientID;
+  clientSecret = keys.googleClientSecret
+}
 
 module.exports = function (passport) {
   passport.use(
     new GoogleStrategy({
-      clientID: process.env.googleClientID,
-      clientSecret: process.env.googleClientSecret,
+      clientID: clientID,
+      clientSecret: clientSecret,
       callbackURL: '/auth/google/callback',
       proxy: true
     }, (accessToken, refreshToken, profile, done) => {
