4e685caadda56533cc214aae04a3bf6e72453280
Kumar Rohit Malhotra <krohitm@gmail.com>
2018-04-22 15:22:31 -0400

    checking heroku config vars

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,14 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
-const keys = require('./keys');
+//const keys = require('./keys');
+
+
 
 module.exports = function (passport) {
   passport.use(
     new GoogleStrategy({
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
+      clientID: process.env.googleClientID,
+      clientSecret: process.env.googleClientSecret,
       callbackURL: '/auth/google/callback',
       proxy: true
     }, (accessToken, refreshToken, profile, done) => {
