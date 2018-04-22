155757f69c5dcdb52f4d1689b379be4b67d5e8ea
Kumar Rohit Malhotra <krohitm@gmail.com>
2018-04-22 14:43:11 -0400

    google callback

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const passport = require('passport');
 
 router.get('/google', passport.authenticate('google',
-  {scope: ['profile', 'email']}))
+  { scope: ['profile', 'email'] }))
+
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/dashboard');
+  });
 
 module.exports = router;
