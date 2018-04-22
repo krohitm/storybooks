45fad9a755d09e0dc9953bf03fa13e195457934f
Kumar Rohit Malhotra <krohitm@gmail.com>
2018-04-22 14:49:56 -0400

    fixing auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds113670.mlab.com:13670/storybooks-dev',
+  googleClientID: '624207174565-nol7les4bl0ifk1oul7u7p827sf699qb.apps.googleusercontent.com',
+  googleClientSecret: 'yvC7B39705dY_-F9yL3J4N5A'
+}
