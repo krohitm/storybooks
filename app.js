746f1145fd8c557923d1545716deafd71e2eef53
Kumar Rohit Malhotra <krohitm@gmail.com>
2018-04-22 14:34:47 -0400

    created google strategy

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport  = require('passport');
+
+//passport config
+require('./config/passport')(passport);
+
+//Load routes
+const auth = require('./routes/auth')
+
 
 const app = express();
 
 app.get('/', (Req, res) => {
   res.send('it works');
 })
+
+app.use('/auth', auth);
 
 //setting port to listen
 const port = process.env.PORT || 5000
