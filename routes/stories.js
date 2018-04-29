c44fe2e09c0206cd26185cffb8b00b7681c30bc4
Kumar Rohit Malhotra <krohitm@gmail.com>
2018-04-29 00:37:14 -0400

    show template and format dates

--- routes/stories.js
+++ routes/stories.js
@@ -17,6 +17,18 @@ router.get('/', (req, res) => {
   })
 })
 
+//show single story
+router.get('/show/:id', (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+  .populate('user')
+  .then(story => {
+    res.render('stories/show', {
+      story: story,
+    })
+  })
+})
 
 //story form
 router.get('/add', ensureAuthenticated, (req, res) => {
