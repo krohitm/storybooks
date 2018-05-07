4b5921d8b43d9545d574a300f774e9026bc95837
Kumar Rohit Malhotra <krohitm@gmail.com>
2018-05-06 23:16:25 -0400

    fetch stories from specific user

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -22,19 +22,14 @@ module.exports = {
       '$& selected="selected"').replace(new RegExp('>' + selected + '</option>'),
         ' selected="selected"$&');
   },
-  editIcon: function (storyUser, loggedUser, storyId, floating = true) {
-    if (storyUser == loggedUser) {
-      if (floating) {
-        return `<a href="/stories/edit/${storyId}" 
-        class="btn-floating halfway-fab red"><i class="fa fa-pencil">
-        </i></a>`;
+  editIcon: function(storyUser, loggedUser, storyId, floating = true){
+    if(storyUser == loggedUser){
+      if(floating){
+        return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab red"><i class="fa fa-pencil"></i></a>`;
+      } else {
+        return `<a href="/stories/edit/${storyId}"><i class="fa fa-pencil"></i></a>`;
       }
-      else {
-        return `<a href="/stories/edit/${storyId}"><i class="fa fa-pencil">
-        </i></a>`;
-      }
-    }
-    else {
+    } else {
       return '';
     }
   }
--- routes/stories.js
+++ routes/stories.js
@@ -28,8 +28,44 @@ router.get('/show/:id', (req, res) => {
     .then(story => {
       res.render('stories/show', {
         story: story,
-      })
+      });
+    });
+});
+
+//list stories from a user
+router.get('/user/:userId', (req, res) => {
+  Story.find({
+    user: req.params.userId,
+    status: 'public'
+  })
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      });
+    });
+});
+
+//list stories from a user
+router.get('/user/:userId', (req, res) =>{
+  Story.find({user: req.params.userId, status: 'public'})
+  .populate('user')
+  .then(stories => {
+    res.render('stories/index', {
+      stories:stories
     })
+  })
+})
+
+//list stories from logged in user
+router.get('/my', ensureAuthenticated, (req, res) =>{
+  Story.find({user: req.user.id})
+  .populate('user')
+  .then(stories => {
+    res.render('stories/index', {
+      stories:stories
+    })
+  })
 })
 
 //add story form
